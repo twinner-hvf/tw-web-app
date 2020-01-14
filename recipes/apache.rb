c155c95ed96cdbfc75b9c68c731a460e2fa83310
twinner-hvf <travis.winner@hitachivantarafederal.com>
2020-01-14 16:28:51 -0500

    first-commit

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -12,15 +12,16 @@ service 'apache2' do
   action [:enable, :start]
 end
 
-cookbook_file '/var/www/html/index.php' do
-  source 'index.php'
+cookbook_file '/var/www/html/index.html' do
+  source 'index.html'
+#  source 'index.php'
   owner 'root'
   group 'root'
   mode '0755'
   action :create
 end
 
-cookbook_file '/var/www/html/index.html' do
-  action :delete
-end
+#cookbook_file '/var/www/html/index.html' do
+#  action :delete
+#end
 
