228c34fd99c4821b4320e99900dd7e18d1783e72
Travis <travis.winner@hitachivantarafederal.com>
2020-01-16 18:44:13 -0500

    commenthere

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -4,6 +4,11 @@
 #
 #
 
+execute "update-upgrade" do
+  command "sudo apt-get update && sudo apt-get upgrade -y"
+  action :run
+end
+
 package ['apache2', 'php', 'php-mysql'] do
   action :install
 end
